b1686729220d3c7a26989c1a0559f9eac9110f96
Weifan Chiang <weifan@formal.cs.utah.edu>
2016-09-12 16:16:39 -0600

    update microbenchmarks

--- examples/micro/reduction.py
+++ examples/micro/reduction.py
@@ -2,9 +2,6 @@
 import tft_ir_api as IR 
 import tft_ir_backend 
 
-
-assert(len(sys.argv) == 5) 
-
 alen     = 512
 vlb      = -100.0 
 vub      = 100.0 
@@ -21,7 +18,7 @@ arr = [IR.RealVE("v"+str(i), gid_data, vlb, vub) for i in range(0, alen)]
 for i_stage in range(1, (n_stages+1)) : 
     l_arr = len(arr) 
     assert(l_arr % 2 == 0) 
-    l_arr = l_arr / 2 
+    l_arr = int(l_arr / 2) 
     
     for i in range(0, l_arr): 
         arr[i] = IR.BE("+", i_stage, arr[i], arr[i+l_arr]) 
