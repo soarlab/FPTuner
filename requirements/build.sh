d88b9c1d9729e234776f4855f9045a40ab1502c2
Ian Briggs <ianbriggsutah@gmail.com>
2020-03-23 15:27:02 -0600

    Making install failures less confusing

--- requirements/build.sh
+++ requirements/build.sh
@@ -9,6 +9,14 @@ SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOG="${SCRIPT_LOCATION}/log.txt"
 rm -f "${LOG}"
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
 function finish {
     if [ "$SUCCESS" == 1 ]
     then
@@ -23,27 +31,36 @@ trap finish EXIT
 
 # FPTaylor with my_sin
 echo "Installing FPTaylor"
-cd "$SCRIPT_LOCATION"
+pushd "$SCRIPT_LOCATION"
 rm -rf FPTaylor
 git clone https://github.com/soarlab/FPTaylor.git &>> "${LOG}"
-cd FPTaylor
+pushd FPTaylor
 git checkout my_sine &>> "${LOG}"
 make &>> "${LOG}"
+popd
+popd
 
 # gelpia
 echo "Installing Gelpia"
-cd "$SCRIPT_LOCATION"
+pushd "$SCRIPT_LOCATION"
 rm -rf gelpia
 git clone https://github.com/soarlab/gelpia.git &>> "${LOG}"
 ln -s FPTuner/gelpia gelpia &>> "${LOG}"
-cd gelpia
-make requirements &>> "${LOG}"
+pushd gelpia
+SUCCESS=-1
+pushd requirements
+./build.sh | sed "s|^|    |g"
+popd
+SUCCESS=1
 make &>> "${LOG}"
+popd
+popd
 
 # Debug enviroment source file
-cd "$SCRIPT_LOCATION"
+pushd "$SCRIPT_LOCATION"
 rm -f debug_eniroment.sh
 echo "export PATH=${SCRIPT_LOCATION}/fptaylor:${SCRIPT_LOCATION}/gelpia/bin:\$PATH" >> debug_eniroment.sh
 echo "export PYTHONPATH=${SCRIPT_LOCATION}/gelpia/bin:\$PYTHON_PATH" >> debug_eniroment.sh
+popd
 
 SUCCESS=0
